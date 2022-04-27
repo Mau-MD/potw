9642cb03f2775fde308edd5d6a6023de415ffc19
Mau MD <j.mauricio.munoz1@gmail.com>
2022-04-27 09:09:42 -0700

    agregando seo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,12 @@
 	import bg from '$lib/assets/hero.jpg';
 </script>
 
+<svelte:head>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://potw.vercel.app" />
+	<meta property="og:title" content="Problemas de la Semana" />
+	<meta property="og:description" content="Tres problemas para resolver a la semana:)" />
+</svelte:head>
 <main>
 	<div
 		class="bg-[#0C1221] text-white h-[100vh] w-[100vw] bg-right overflow-x-hidden"
